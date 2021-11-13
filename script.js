efcc063a06b24d5c52030025e874598a66f268df
xvtlnx <65411175+xvtlnx@users.noreply.github.com>
2021-11-13 15:54:50 +0200

    first

---
+++ script.js
@@ -0,0 +1,21 @@
+const textArea = document.getElementById("areaText");
+const speedArea = document.getElementById("areaSpeed");
+const outputArea = document.getElementById("output");
+const btn = document.getElementById("btn");
+btn.addEventListener("click", start);
+function start() {
+    if (speedArea.value == '' || isNaN(speedArea.value) || textArea.value == '') {
+        return;
+    }
+    let words = (textArea.value + " End.").split(" ");
+    let speed = speedArea.value;
+    let i = { value: 0 };
+    function output() {
+        if (words[i.value] == undefined) {
+            return;
+        }
+        outputArea.innerText = words[i.value];
+        i.value++;
+    }
+    setInterval(output, parseInt(60 / speed * 1000));
+}
